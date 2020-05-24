fe3c5bc2077e2dcbfdfd87d2cc154ecdac3865d5
Akande Benji <adeoul001@gmail.com>
2020-05-23 23:50:23 -0700

    Sanitize and Security Updates

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+
 const app = express();
 
 const morgan = require('morgan');
@@ -8,14 +14,46 @@ const tourRouter = require('./routes/tourRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require("./controllers/errorController")
 
+
+//GLOBAL MIDDLEWARE
+// Set security HTTPS
+app.use(helmet())
+
+// Document logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+
+// Limit req from  the same API
+const limiter = rateLimit({
+  max:100,
+  windowMs:60*60*1000,
+  message:'Too many Requests from this IP please try again in an hour'
+})
+app.use('/api',limiter)
+
+
+// Body parser, reading data from body into req.body
+app.use(express.json({limit:'10kb'}));
+
+// Data sanitization agaist NoSQL query Injection
+app.use(mongoSanitize())
+
+// Data sanitization against Cross site scripting attacks (XXS)
+app.use(xss())
+// Serving static files
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist:['duration','ratingsQuantiy','ratingsAverage','maxGroupSize','difficulty','price']
+}))
+
+
 app.use(express.static(`${__dirname}/public`));
 
-//CREATING MIDDLEWARE
+
+// Test middlewares
 app.use((req, res, next) => {
   next();
 });
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -25,6 +25,8 @@ class APIFeatures {
 
   sort() {
     if (this.queryString.sort) {
+      console.log(this.queryString.sort)
+
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
